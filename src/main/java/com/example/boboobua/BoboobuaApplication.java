dc4ce512a11ddfb4ebd9b365d081fa9e78091872
uthai <uthai.u@kbtg.tech>
2019-09-16 13:47:57 +0700

    First

---
+++ src/main/java/com/example/boboobua/BoboobuaApplication.java
@@ -0,0 +1,13 @@
+package com.example.boboobua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoboobuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoboobuaApplication.class, args);
+	}
+
+}
